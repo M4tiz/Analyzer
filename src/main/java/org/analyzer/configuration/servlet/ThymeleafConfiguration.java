d968000918af4912e04654d9065b415c2dfd052e
mrutski <mateusz.rutski@sagiton.pl>
2015-01-24 13:22:08 +0100

    Configuration

--- src/main/java/org/analyzer/configuration/servlet/ThymeleafConfiguration.java
+++ src/main/java/org/analyzer/configuration/servlet/ThymeleafConfiguration.java
@@ -1,9 +1,21 @@
 package org.analyzer.configuration.servlet;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ApplicationObjectSupport;
 import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.dialect.IDialect;
+import org.thymeleaf.extras.springsecurity3.dialect.SpringSecurityDialect;
+import org.thymeleaf.messageresolver.IMessageResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.messageresolver.SpringMessageResolver;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import java.util.Set;
 
 /**
  * @author mateusz.rutski@sagiton.pl
@@ -13,7 +25,67 @@ import org.springframework.web.servlet.ViewResolver;
 public class ThymeleafConfiguration extends ApplicationObjectSupport {
 
     @Bean
-    public ViewResolver viewResolver() {
-        return null;
+    public ViewResolver viewResolver(SpringTemplateEngine springTemplateEngine) {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+
+        resolver.setTemplateEngine(springTemplateEngine);
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setOrder(1);
+
+        return resolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine springTemplateEngine(Set<? extends ITemplateResolver> templateResolvers, IMessageResolver messageResolver, IDialect dialect) {
+
+        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+
+        springTemplateEngine.setDialect(dialect);
+        springTemplateEngine.setMessageResolver(messageResolver);
+        springTemplateEngine.setTemplateResolvers(templateResolvers);
+
+        return springTemplateEngine;
+    }
+
+    @Bean
+    public IMessageResolver springMessageResolver(MessageSource messageSource) {
+        SpringMessageResolver springMessageResolver = new SpringMessageResolver();
+
+        springMessageResolver.setMessageSource(messageSource);
+
+        return springMessageResolver;
+    }
+
+    @Bean
+    public IDialect springSecurityDialect() {
+        return new SpringSecurityDialect();
+    }
+
+    @Bean
+    public ITemplateResolver servletTemplateResolver() {
+        ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+
+        resolver.setPrefix("/WEB-INF/views");
+        resolver.setOrder(10);
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setCacheable(false);
+
+        return resolver;
+    }
+
+    @Bean ITemplateResolver deployTemplateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+
+        resolver.setPrefix("classpath:/views/");
+        resolver.setOrder(20);
+        resolver.setSuffix(".html");
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setTemplateMode("HTML5");
+        resolver.setCacheable(true);
+        resolver.setCacheTTLMs(1000L);
+
+        return resolver;
     }
 }
---
+++ src/main/java/org/analyzer/configuration/servlet/WebMVCConfiguration.java
@@ -0,0 +1,72 @@
+package org.analyzer.configuration.servlet;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+/**
+ * @author mateusz.rutski@sagiton.pl
+ */
+
+@Configuration
+@EnableWebMvc
+public class WebMVCConfiguration extends WebMvcConfigurerAdapter {
+
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("/static");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login/login");
+        registry.addViewController("/logout/success").setViewName("login/login");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+
+        localeChangeInterceptor.setParamName("language");
+
+        return localeChangeInterceptor;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+
+        sessionLocaleResolver.setDefaultLocale(new Locale("en"));
+
+        return sessionLocaleResolver;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasename("classpath*:/i18n/messages");
+        messageSource.setCacheSeconds(0);
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setFallbackToSystemLocale(false);
+        messageSource.setUseCodeAsDefaultMessage(true);
+
+        return messageSource;
+    }
+}
